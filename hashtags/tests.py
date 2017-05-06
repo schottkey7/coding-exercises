4f71134ff0f34ac85f1f478a449b90f303cc61a8
Nora Petrova <schottkey@gmail.com>
2017-05-07 00:55:14 +0100

    test: Added some tests

--- hashtags/tests.py
+++ hashtags/tests.py
@@ -1,52 +1,66 @@
+
 from controller import app, db
-from utils import shorten_url
-from models import *
+import helper
+import models
 import unittest
+from uuid import uuid4
 
 
-class FlaskURLShortenerTester(unittest.TestCase):
+class HashtagsTestCases(unittest.TestCase):
+    """Test for API endpoints"""
 
-    def test_index(self):
-        """ Test getting the home page """
-        tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+        # drop all existing
+        db.drop_all()
+        # create the database and db tables
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+
+    def test_home(self):
+        """Test getting the home page"""
+        response = self.app.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
-    def test_report(self):
-        """ Test getting the report page """
-        tester = app.test_client(self)
-        response = tester.get('/report', content_type='html/text')
+    def test_process(self):
+        """Test posting data to be processed"""
+        response = self.app.post('/process', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
-    def test_shorten(self):
-        """ Test the shorten function """
-        url = "http://example.com"
-        size = 5
-        self.assertEqual(len(shorten_url(url, size)), size)
+    def test_get_all_db_docs(self):
+        results = helper.get_all_db_docs()
+        self.assertIsInstance(results, list)
 
-    def test_redirect(self):
-        """ Test general redirect on /shorten """
-        tester = app.test_client(self)
-        response = tester.get('/shorten', content_type='html/text')
-        # Should return 301 moved permanently
-        self.assertEqual(response.status_code, 301)
+    def test_insert_document(self):
+        doc_id = helper.insert_document('{}.txt'.format(uuid4()))
+        self.assertIsInstance(doc_id, int)
 
-    def test_retrieve_non_existent(self):
-        """ Test GET on non existing shortened URL """
-        url = "nonexistenttinyurl"
-        tester = app.test_client(self)
-        response = tester.get('/' + url, content_type='html/text')
-        # Should return 404 not found
-        self.assertEqual(response.status_code, 404)
+    def test_process_raw(self):
+        contents = 'This is a test string'
+        document_words = helper.process_raw(contents, 15)
+        self.assertEqual(document_words, {'test': 1, 'string': 1})
 
-    def test_retrieve_extant(self):
-        """ Fetch an existing URL """
-        existing_url = db.session.query(Url).first()
-        tester = app.test_client(self)
-        response = tester.get('/' + existing_url.shortened, content_type='html/text')
-        # Should return 302 moved permanently after finding existing url
-        self.assertEqual(response.status_code, 302)
+    def test_document_model(self):
+        doc = models.Document()
+        self.assertEqual(doc.__tablename__, 'Document')
+        self.assertIsInstance(doc.query.all(), list)
 
+    def test_document_words_model(self):
+        doc = models.DocumentWords()
+        self.assertEqual(doc.__tablename__, 'DocumentWords')
+        self.assertIsInstance(doc.query.all(), list)
+
+    def test_sentence_words_model(self):
+        doc = models.SentenceWords()
+        self.assertEqual(doc.__tablename__, 'SentenceWords')
+        self.assertIsInstance(doc.query.all(), list)
 
 if __name__ == '__main__':
     unittest.main()
