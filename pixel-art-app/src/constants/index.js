7992756a60c35773deba10e6ad5aa3d0be8c11c1
Nora Petrova <schottkey@gmail.com>
2017-05-21 12:29:29 +0100

    style: Move initial state to its own file

---
+++ pixel-art-app/src/constants/index.js
@@ -0,0 +1,23 @@
+import { List as list, Map as map } from 'immutable';
+
+import { matrix } from '../utils';
+
+
+const defaultGridSize = 20;
+const defaultCellColor = '#fbfbfb';
+
+const initialState = {
+
+    boards: list([map({
+        board: matrix(
+            defaultGridSize,
+            defaultGridSize,
+            defaultCellColor
+        )
+    })])
+};
+
+
+export {
+    initialState
+};
--- pixel-art-app/src/index.js
+++ pixel-art-app/src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { List as list, Map as map } from 'immutable';
 
 import App from './App';
 import reducer from './reducers';
-import { matrix } from './utils';
+import { initialState } from './constants';
 import './index.css';
-
-
-const initialState = {
-    boards: list([map({
-        board: matrix(30, 30, '#000')
-    })])
-};
 
 const store = createStore(reducer, initialState);
 
